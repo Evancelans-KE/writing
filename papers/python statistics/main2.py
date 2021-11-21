81a52c36f39e0528b4550884bc360ededda51336
Evance Onyango <evaonyango251@gmail.com>
2021-11-21 17:58:16 +0300

    dded p t

---
+++ papers/python statistics/main2.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+from sklearn import datasets, linear_model
+from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
+from scipy import stats
+
+data = pd.read_csv (r'salroe.csv')
+X = data.salary
+y =data.roe
+
+X2 = sm.add_constant(X)
+est = sm.OLS(y, X2)
+est2 = est.fit()
+print(est2.summary())
