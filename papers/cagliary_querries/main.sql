649f4ee6f00f5675a2d606effd41637a5c320bf6
Evance Onyango <evaonyango251@gmail.com>
2021-12-04 14:13:42 +0300

    added covid 19 infograph

--- papers/cagliary_querries/main.sql
+++ papers/cagliary_querries/main.sql
@@ -1,7 +1,10 @@
-select  median(grade_1),median(grade_2),median(grade_3),median(grade_4),median(grade_5),median(grade_6),median(grade_8),
-median(grade_8),median(grade_9),median(grade_10),median(grade_11),median(grade_12),
-avg(grade_1),avg(grade_2),avg(grade_3),avg(grade_4),avg(grade_5),avg(grade_6),avg(grade_8),avg(grade_8),avg(grade_9),
-avg(grade_10),avg(grade_11),avg(grade_12)
-from school_e_data
-where school_year ='2014_2015' and school_year = '2019-2020'
-group by grade1_grade_2,grade_3,grade_4,grade_5,grade_6,grade_8,grade_8,grade_9,grade_10,grade_11,grade_12
+ select * from temp_tbl_2bd_failed_bnks;
+ ---
+select closing_date, count(*) from temp_tbl_2bd_failed_bnks
+group by closing_date 
+order by closing_date desc 
+
+-----
+select bank, updated_date from temp_tbl_2bd_failed_bnks
+where closing_date >='2012-01-01'
+order by updated_date desc 
