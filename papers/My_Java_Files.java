02672001b9262068ea39547277c807b3e3115b28
Evance Onyango <evaonyango251@gmail.com>
2022-02-03 07:24:49 +0300

    added java basic ATM code

--- papers/My_Java_Files.java
+++ papers/My_Java_Files.java
@@ -1,4 +1,3 @@
-//Simple number identifications;
 package com.enaji;
 
 import java.util.Date;
@@ -9,18 +8,31 @@ import static java.time.LocalTime.now;
 public class Main {
 
     public static <scanner> void main(String[] args) {
-        Scanner sc = new Scanner(System.in); //this class captures inputs
-        double avg, sum, div, multiplication; //the operations we want to carry out;
-        int currentYear = 2022;
-        System.out.println("Enter your Current  age: ");
-        int Age = sc.nextInt();
-        int birthYear = currentYear - Age;
-        String greeting = "Welcome to GT Bank";
-        System.out.println(greeting + "Your Year of Birth is" + birthYear);
+        Scanner sc = new Scanner(System.in); //this class captures input
+        System.out.println("Enter your PIN: ");
+        int userPin = sc.nextInt();
 
+         //Define ATM activities
+        String one =    "1. Check Balance";
+        String two =    "2. Withdraw";
+        String three =  "3. Deposit";
+        int currentBalance = 30_000;
+        String allActions = one + two + three;
+        System.out.println(allActions);
+        System.out.println("Type the  Service Type below:");
+        String userAction = sc.nextLine();
 
+       //show user balance
+          if (userAction == one){
+              System.out.println(currentBalance);
+          }
+          //Deduct balance from user account;
+          else if (userAction == two){
+              System.out.println("Enter Amount to Withdraw: ");
+              int withdrawAmount = sc.nextInt();
+              //then deduct from balance
+              float deductfromBalance = currentBalance - withdrawAmount;
+              System.out.println("Withdraw Successful, Your balance is ..." + deductfromBalance);
+          }
     }
 }
-
-// BASIC ATM  SCENERIO
-
